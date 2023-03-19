09b523b48aa675aaf4a41ccb16e77d7d04a22c70
Kayleigh Duncanwq <pattonsgirl@gmail.com>
2023-03-19 11:54:37 -0400

    demos from 3/17

---
+++ my-world/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello from Kayleigh Duncan's world!");
+   return 0;
+}
---
+++ pyproj/code.py
@@ -0,0 +1,5 @@
+#! /usr/local/bin/python
+
+print("Hello World")
+print("More text!")
+print(f"2+4 is {2+4}")  
